19357c29d79fd164a6de40027bb4dff1b7b81d18
Diego Sierra <nirvanaponal007>
2024-04-17 16:21:20 -0500

    crear migraciones y superusuario

--- natural_store/settings.py
+++ natural_store/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +123,7 @@ STATIC_URL = 'static/'
 #nueva constante
 
 STATICFILES_DIRS = (
+
     os.path.join(BASE_DIR, 'static'),
 )
 
