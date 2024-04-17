34cf870fe517cfc28721e5764c934d5e672d820d
Diego Sierra <nirvanaponal007>
2024-04-17 15:30:42 -0500

    integracion con bootstrap

--- natural_store/views.py
+++ natural_store/views.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 
 
 def index (request):
-    return render (request, 'index.html' , {})
+    return render (request, 'index.html' , {
+        'mensaje': 'Hola Tio desde el mensaje',
+        'titulo_empresa': 'Natural Star B',
+        'productos':[
+            {'titulo':'Shampoo Coco','precio':35000,'inventario': True},
+            {'titulo':'Shampoo Manzana','precio':45000,'inventario': True},
+            {'titulo':'Shampoo Pera','precio':37000,'inventario': False},
+        ]
+    })
